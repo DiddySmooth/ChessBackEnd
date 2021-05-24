0d47e8ada08e2b9ada0a92b3a0a0d60c45d3fb10
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2021-05-24 16:49:01 -0500

    socket

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
+
+const http = require('http')
+const socket = require('socket.io')
+
 require('dotenv').config()
+
+const server = http.createServer(app, {
+  cors:{
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST"]
+  }
+})
+const io = socket(server)
 
 app.use(require('morgan')('tiny'))
 const routesReport = require('rowdy-logger').begin(app)
@@ -9,7 +22,7 @@ app.use(express.json())
 app.use(require('cors')())
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
   routesReport.print()
 })
@@ -17,3 +30,19 @@ app.listen(PORT, () => {
 const userRouter = require('./routes/userRoutes')
 
 app.use('/user', userRouter)
+
+var players;
+var games = Array(100);
+for (let i = 0; i < 100; i++) {
+    games[i] = {players: 0 , pid: [0 , 0]}
+}
+
+io.on('connection', function (socket) {
+  var playerId = Math.floor((Math.random() * 100) + 1)
+  console.log(playerId + ' connected')
+
+  socket.on('disconnect', function () {
+    console.log(playerId + ' disconnected')
+  })
+})
+
